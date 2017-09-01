8f1584da85349ecf93527d129362238611b793bc
Bruce Baron <bbaron@enterrasolutions.com>
2017-09-01 14:39:34 -0400

    v2

--- src/App.js
+++ src/App.js
@@ -9,10 +9,45 @@ const PLACES = [
 ];
 
 class WeatherDisplay extends Component {
+  constructor() {
+    super();
+    this.state = {
+      weatherData: null
+    };
+  }
+
+  componentDidMount() {
+    const zip = this.props.zip;
+    const URL = "http://api.openweathermap.org/data/2.5/weather?q=" +
+      zip +
+      "&appid=b1b35bba8b434a28a0be2a3e1071ae5b&units=imperial";
+    fetch(URL)
+      .then(res => res.json())
+      .then(json => {
+        this.setState({weatherData: json});
+      });
+  }
+
   render() {
+    const weatherData = this.state.weatherData;
+    if (!weatherData) return <div>Loading</div>;
+    const weather = weatherData.weather[0];
+    console.log(weatherData);
+    console.log(weather);
+    console.log('weather.main = ' + weather.main);
+    const iconUrl = "http://openweathermap.org/img/w/" + weather.icon + ".png";
     return (
-      <h1>Displaying weather for city {this.props.zip}</h1>
-    )
+      <div>
+        <h1>
+          {weather.main} in {weatherData.name}
+          <img src={iconUrl} alt={weatherData}/>
+        </h1>
+        <p>Current: {weatherData.main.temp}°</p>
+        <p>High: {weatherData.main.temp_max}°</p>
+        <p>Low: {weatherData.main.temp_min}°</p>
+        <p>Wind Speed: {weatherData.wind.speed} mi/hr</p>
+      </div>
+    );
   }
 }
 
